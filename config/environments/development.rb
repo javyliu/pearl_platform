5547167170ed1b628938a7341f9ffc3094f500ab
qmliu <qmliu@pearlinpalm.com>
2019-11-04 13:07:54 +0800

    fix bug for erb in vm did't refresh

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # FileUpdateChecker will detect by polling the change of the file.
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
